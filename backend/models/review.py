705855fefef128ac831f78996e571d61a5df6449
Chaitanya Patil <chaitanyapatil.xe@gmail.com>
2026-02-16 19:14:22 +0530

    ATS-10: Setup data models

---
+++ backend/models/review.py
@@ -0,0 +1,15 @@
+from app.extensions import db
+from datetime import datetime
+
+class ApplicationReview(db.Model):
+    __tablename__ = 'application_reviews'
+
+    review_id = db.Column(db.Integer, primary_key=True)
+    
+    # Foreign Keys
+    applicant_id = db.Column(db.Integer, db.ForeignKey('applicants.applicant_id'), nullable=False)
+    admin_id = db.Column(db.Integer, db.ForeignKey('admins.admin_id'), nullable=False)
+    
+    status = db.Column(db.String(30), default="Pending") # Pending/Shortlisted/Rejected
+    comments = db.Column(db.Text)
+    reviewed_at = db.Column(db.DateTime, default=datetime.utcnow)
